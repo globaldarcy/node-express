914c3b5e76ca74e98b2e53661edb1191485596f4
Shawn <289904316@qq.com>
2017-04-27 10:02:19 +0800

    lesson 4 end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fortune = require('../lib/fortune');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/about', function(req, res) {
+  res.render('about', { fortune: fortune.getFortune() });
+});
+
+
+
+
+
+
 
 module.exports = router;
